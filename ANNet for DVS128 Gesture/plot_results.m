4cd2160fd9ddd6a0755bc3805934aa7d604df5a6
Amélie Gruel <ameliegruel@free.fr>
2021-02-26 15:47:15 +0100

    modified plots + first results for DVS Gesture cat 4

--- ANNet for DVS128 Gesture/plot_results.m
+++ ANNet for DVS128 Gesture/plot_results.m
@@ -1,3 +1,17 @@
-plotSpikeTrain(spikeTrainAN,start,stop)
+figure;
+suptitle({"Simulation results for DVS128 Gesture over "+int2str(stop)+" ms","Hand gesture category : right hand counter clockwise (5)"});
+subplot(3,1,1);
+plotSpikeTrain(spikeTrainAN,start,stop);
+ylabel("Attention neuron");
+hold off;
+subplot(3,1,2);
+plotSpikeTrain(spikeTrainL1,start,stop);
+ylabel("Intermediate layer");
+hold off;
+subplot(3,1,3);
+plotSpikeTrain(spikeTrainL2,start,stop),
+ylabel("Output layer");
+xlabel("Time (in ms)");
+
 % plotPotHist(L1PotHist,step)
 % plotPotHist(L1ThHist,step)
