c32ed9079da692854dd8fa8f2371d14f29d1b211
Amélie Gruel <ameliegruel@free.fr>
2021-03-02 14:48:02 +0100

    updated to get one or more sample from categories specified as input - each sample is randomly selected in given category

--- Attention model with Matlab - Bernert and Yvert (2019) adapted to DVS128 Gesture/transform_DVS_Gesture.py
+++ Attention model with Matlab - Bernert and Yvert (2019) adapted to DVS128 Gesture/transform_DVS_Gesture.py
@@ -4,6 +4,11 @@ import tonic.transforms as tr
 import numpy as np
 import csv
 from skimage.measure import block_reduce
+import sys
+
+if len(sys.argv) == 1:
+    print("Usage :  python transform_DVS_Gesture.py [category of the sample(s) to load]")
+    sys.exit()
 
 dl = not os.path.isfile('data_gesture/gesture.zip')
 
@@ -16,33 +21,55 @@ for ev,target in iter(loader):
     print(target)
 print(nb)
 """
-ev,target=next(iter(loader))
-print("Target :", target)
-events = ev.numpy()
 
-print("Shape du sample :",events.shape)
-print("Sensor size du dataset :",gesture.sensor_size)
+print("### Data loaded ###\n")
 N = np.prod(gesture.sensor_size)
-index_x = gesture.ordering.find("x") 
-index_y = gesture.ordering.find("y")
-index_t = gesture.ordering.find("t")
+print("Sensor size du dataset :",gesture.sensor_size)
+print("Nombre de pixels :",N)  # len(spikes) = 16384 = 128*128
+print()
 
-spikes = []
+start_timestamps = 0
+spikes = [[] for pixel in range(int(N/4))]
+event_by_sample = []
+
+for sample in sys.argv[1:]:
+    print("// SAMPLE for category "+sample+" //")
+    for ev,target in iter(loader):
+        if target.item() == int(sample) and ev.numpy().shape[1] not in event_by_sample: # we want right hand clockwise
+            break
+
+    events = ev.numpy()
+    print("Target :", target.item())
+    print("Number of events :", events.shape[1])
+    print("Shape du sample :",events.shape)
+    event_by_sample.append(events.shape[1])
+
+    index_x = gesture.ordering.find("x") 
+    index_y = gesture.ordering.find("y")
+    index_t = gesture.ordering.find("t")
+
+    for pixel in range(int(N/4)): 
+        row = pixel // (gesture.sensor_size[0])
+        col = pixel %  (gesture.sensor_size[1])
+        spike_idx = np.where((events[0, :, index_x]//2 == row) & (events[0, :, index_y]//2 == col))[0]
+        spike_times = [start_timestamps + int(round(e)) for e in list(events[0, spike_idx, index_t] * 1e-3)] # in milliseconds
+        spikes[pixel] += spike_times
+
+    # print(np.sum([len(st) for st in spikes]))
+    # idx = ev[0, 0, index_x]*(gesture.sensor_size[0]) + ev[0, 0, index_y] # first spike
+    
+    print("Plus grand timestamp :",max(sum(spikes, []))-start_timestamps)
+    start_timestamps = max(sum(spikes,[]))
+    print("")
+
+
+print("Writing file...")
 gesture_data = open("data/gesture_data.csv","w")
-for i in range(int(N/4)): 
-    row = i // (gesture.sensor_size[0])
-    col = i %  (gesture.sensor_size[1])
-    spike_idx = np.where((events[0, :, index_x]//2 == row) & (events[0, :, index_y]//2 == col))[0]
-    spike_times = [int(round(e)) for e in list(events[0, spike_idx, index_t] * 1e-3)] # in milliseconds
-    spikes.append(spike_times)
-    gesture_data.write(";".join([str(e) for e in spike_times]))
+for pixel in spikes:
+    gesture_data.write(";".join([str(e) for e in pixel]))
     gesture_data.write(";\n")
-
 gesture_data.close()
+
 os.system('csvtool transpose -t ";" -u ";" data/gesture_data.csv > data/T_gesture_data.csv')
 os.system('mv data/T_gesture_data.csv data/gesture_data.csv')
-
-print(np.sum([len(st) for st in spikes]))
-# idx = ev[0, 0, index_x]*(gesture.sensor_size[0]) + ev[0, 0, index_y] # first spike
-print("Nombre de pixels :",len(spikes))  # len(spikes) = 16384 = 128*128
-print("Plus grand timestamp :",max(sum(spikes, [])))
+print("Sample(s) have been saved as data/gesture_data.csv")
