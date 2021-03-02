e63a3ced207d5e1ba749aacf25160e6518b5a4b4
Amélie Gruel <43147713+amygruel@users.noreply.github.com>
2021-03-02 15:24:46 +0100

    Create README.m

---
+++ Attention model with Matlab - Bernert and Yvert (2019) adapted to DVS128 Gesture/README.m
@@ -0,0 +1,10 @@
+# Visual Attention for Event Cameras based on Spiking Neural Networks
+
+This work consists in the adaptation of the Matlab script produced by Bernert and Yvert (2019) to the DVS128 Gesture dataset. 
+
+## How to run the script ? 
+
+  
+## Requirements
+  
+`Matlab` should be installed under the version 2018b at least. 
