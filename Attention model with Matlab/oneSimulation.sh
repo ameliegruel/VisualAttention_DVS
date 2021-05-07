27bc2d82695b18e19bda86277a361797f80f6609
Amélie Gruel <ameliegruel@free.fr>
2021-05-07 15:22:12 +0200

    modification of oneSimulation to produce and save the accuracy of the results using getAccuracy.py

--- Attention model with Matlab/oneSimulation.sh
+++ Attention model with Matlab/oneSimulation.sh
@@ -22,7 +22,12 @@ nbOutputNeurons=11
 
 # save parameters
 touch Results/Simu_$now/Parameters_Simu_$now.csv
-echo "Simulation;Number of active output neurons;Activation rate of output layer;Rate coding;Rank order coding - Accuracy;Rank order coding - Specificity;threshold Attention;tm Attention;threshold Intermediate;tm Intermediate;neurons Intermediate;threshold Output;tm Output;neurons Output;" > Results/Simu_$now/Parameters_Simu_$now.csv
+
+# When accuracy run with Matlab script: 
+#   echo "Simulation;Number of active output neurons;Activation rate of output layer;Rate coding;Rank order coding - Accuracy;Rank order coding - Specificity;threshold Attention;tm Attention;threshold Intermediate;tm Intermediate;neurons Intermediate;threshold Output;tm Output;neurons Output;" > Results/Simu_$now/Parameters_Simu_$now.csv
+
+# When accuracy run with Scikit-learn:
+echo "Simulation;Rate coding;Rank order coding;Latency coding;threshold Attention;tm Attention;threshold Intermediate;tm Intermediate;neurons Intermediate;threshold Output;tm Output;neurons Output;" > Results/Simu_$now/Parameters_Simu_$now.csv
 
 # define matlab commands 
 import_data="gesturedata=importdata('data/gesture_data.csv');samplestime=importdata('data/samples_time.csv');save('data/gesture_data.mat','gesturedata','-v7.3');"
@@ -30,7 +35,7 @@ Attention_parameters="SIMU.DN_tm=$tmAttention;SIMU.DN_threshold=$thresholdAttent
 Intermediate_parameters="SIMU.L1_tm=$tmInter;SIMU.L1_threshold=$thresholdInter;SIMU.L1_nNeurons=$nbInterNeurons;"
 Output_parameters="SIMU.L2_tm=$tmOutput;SIMU.L2_maxTh=$thresholdOutput;SIMU.L2_nNeurons=$nbOutputNeurons;"
 run_scripts="run('Attention_Neuron/runScriptDN.m');run('Intermediate_Layer/runScriptL1.m');run('Output_Layer/runScriptL2.m');"
-run_accuracy="run('compute_accuracy.m');"
+run_accuracy="" # "run('compute_accuracy.m');"
 exit="exit;"
 
 
@@ -96,7 +101,6 @@ echo "timestamps;neuron tags" >> Results/Simu_$now/Simu$s/OutputData_Simu$s.csv
 
 # matlab command 
 run_plots="addpath('./drawingUtils');run('plot_results.m');saveas(gcf,'Results/Simu_$now/Simu$s/Output_Simu$s.png');saveas(gcf,'Results/Simu_$now/Simu$s/Output_Simu$s.fig');close(gcf);"
-# run_plots="addpath('./drawingUtils');run('plot_results.m');close(gcf);"
 
 # run matlab 
 /usr/local/MATLAB/R2018a/bin/matlab -nosplash -nodesktop -r $import_data$simu_parameters$Attention_parameters$Intermediate_parameters$Output_parameters$run_scripts$run_plots$run_accuracy$exit | tee tmp.txt
@@ -104,13 +108,22 @@ run_plots="addpath('./drawingUtils');run('plot_results.m');saveas(gcf,'Results/S
 # save results
 sed -n '/OutputData/,/done/p' tmp.txt | grep '[0-9]' | sed 's/  */;/g' | sed 's/^;//g' >> Results/Simu_$now/Simu$s/OutputData_Simu$s.csv
 
-# save parameters
-nbActiveOutputNeurons=$(grep "activated Output" tmp.txt | grep -o "[0-9]*")
-rateActiveOutput=$(grep "Percentage of Output layer activation" tmp.txt | grep -o "[0-9]\.*[0-9]*")
-rateCoding=$(grep "Accuracy - rate coding" tmp.txt | grep -o "[0-9]\.*[0-9]*")
-rankOrderAccuracy=$(grep "Accuracy - rank order coding" tmp.txt | grep -o "[0-9]\.*[0-9]*")
-rankOrderSpecificity=$(grep "Specificity - rank order coding" tmp.txt | grep -o "[0-9]\.*[0-9]*")
-echo "$s;$nbActiveOutputNeurons;$rateActiveOutput;$rateCoding;$rankOrderAccuracy;$rankOrderSpecificity;$thresholdAttention;$tmAttention;$thresholdInter;$tmInter;$nbInterNeurons;$thresholdOutput;$tmOutput;$nbOutputNeurons;" >> Results/Simu_$now/Parameters_Simu_$now.csv
+# get accuracy and save parameters
+
+# When accuracy run with Matlab script:
+#    nbActiveOutputNeurons=$(grep "activated Output" tmp.txt | grep -o "[0-9]*")
+#    rateActiveOutput=$(grep "Percentage of Output layer activation" tmp.txt | grep -o "[0-9]\.*[0-9]*")
+#    rateCoding=$(grep "Accuracy - rate coding" tmp.txt | grep -o "[0-9]\.*[0-9]*")
+#    rankOrderAccuracy=$(grep "Accuracy - rank order coding" tmp.txt | grep -o "[0-9]\.*[0-9]*")
+#    rankOrderSpecificity=$(grep "Specificity - rank order coding" tmp.txt | grep -o "[0-9]\.*[0-9]*")
+#    echo "$s;$nbActiveOutputNeurons;$rateActiveOutput;$rateCoding;$rankOrderAccuracy;$rankOrderSpecificity;$thresholdAttention;$tmAttention;$thresholdInter;$tmInter;$nbInterNeurons;$thresholdOutput;$tmOutput;$nbOutputNeurons;" >> Results/Simu_$now/Parameters_Simu_$now.csv
+
+# When accuracy run with Scikit learn:
+rateCodingAccuracy=$(grep "Rate" tmp.txt | grep -o "[0-9]\.[0-9]*")
+rankOrderCodingAccuracy=$(grep "Rank" tmp.txt | grep -o "[0-9]\.[0-9]*")
+latencyCodingAccuracy=$(grep "Latency" tmp.txt | grep -o "[0-9]\.[0-9]*")
+echo "$s;$rateCodingAccuracy;$rankOrderCodingAccuracy;$latencyCodingAccuracy;$thresholdAttention;$tmAttention;$thresholdInter;$tmInter;$nbInterNeurons;$thresholdOutput;$tmOutput;$nbOutputNeurons;" >> Results/Simu_$now/Parameters_Simu_$now.csv
+
 rm tmp.txt
 
 echo "Simulation done"
